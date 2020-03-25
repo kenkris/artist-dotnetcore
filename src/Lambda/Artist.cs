85725fcc1a464cc1b484059ce00f1517d389b55f
Kenneth Hoejfeldt Kristensen <dkkehokr@lego.com>
2020-03-25 07:10:41 +0100

    WIP GetArtist. Now it runs in cloud but no data in result yet

---
+++ src/Lambda/Artist.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+
+namespace Lambda
+{
+    public class Artist
+    {
+        private AmazonDynamoDBClient _dbClient = new AmazonDynamoDBClient();
+        
+        public APIGatewayProxyResponse GetArtist(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            // TODO determine getAll or byId
+            var result = _getAllArtist();
+                
+            return new APIGatewayProxyResponse
+            {
+                Body = JsonConvert.SerializeObject(result),
+                StatusCode = 200,
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+            };
+        }
+
+        private List<ArtistModel> _getAllArtist()
+        {
+            var query = new QueryRequest
+            {
+                TableName = "ArtistDB2",
+                KeyConditionExpression = "sk_gsi_pk = :artistStatic",
+                ExpressionAttributeValues = new Dictionary<string, AttributeValue>
+                {
+                    { ":artistStatic", new AttributeValue { S = "Artist" } }
+                }
+            };
+            var queryResult = _dbClient.QueryAsync(query);
+
+            /*foreach (var item in queryResult.Result)
+            {
+                Console.Out.WriteLine(item.Values);
+            }*/
+            
+            return new List<ArtistModel>();
+        }
+
+        private ArtistModel _getArtistById(string id)
+        {
+            return new ArtistModel();
+        }
+    }
+
+    [DynamoDBTable("ArtistDB2")]
+    public class ArtistModel
+    {
+        [DynamoDBHashKey] public string pk;
+        [DynamoDBHashKey, DynamoDBRangeKey]public string sk_gsi_pk;
+        [DynamoDBRangeKey] public string data_gsi_sk;
+        public string name;
+    }
+}
